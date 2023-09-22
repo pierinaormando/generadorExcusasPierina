79226e0113a1d595792ddbc82e40140af952819b
Pierina Ormando <110559878+pierinaormando@users.noreply.github.com>
2023-09-22 20:14:29 +0000

    commit

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+    const action = ['ate', 'peed', 'crushed', 'broke'];
+    const what = ['my homework', 'the keys', 'the car'];
+    const when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+
+    const generateExcuseButton = document.getElementById('generateExcuse');
+    const excuseParagraph = document.getElementById('excuse');
+
+    function generateRandomExcuse() {
+        const randomWho = who[Math.floor(Math.random() * who.length)];
+        const randomAction = action[Math.floor(Math.random() * action.length)];
+        const randomWhat = what[Math.floor(Math.random() * what.length)];
+        const randomWhen = when[Math.floor(Math.random() * when.length)];
+
+        const excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+        return excuse;
+    }
+
+    generateExcuseButton.addEventListener('click', function () {
+        const randomExcuse = generateRandomExcuse();
+        excuseParagraph.innerHTML = randomExcuse;
+    });
+});
+
+
+
+
+
